c7385b5dc4a2301a4a04190d4b9cc56a8ce95655
Ethan Chien <liangtie.qian@gmail.com>
2024-06-24 23:44:35 +0800

    update script

--- start_all.sh
+++ start_all.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$APP_DIR/scripts"
 
 nohup python3 "$SCRIPT_DIR/cli_srv.py" &
 nohup python3 "$SCRIPT_DIR/file_srv.py" &
-nohup python3 -m http.server -d $APP_DIR 8012 &
-
 echo "Viewer is running at http://localhost:8012"
+python3 -m http.server -d $APP_DIR 8012
+
